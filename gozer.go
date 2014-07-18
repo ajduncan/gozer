91ed3337a7cacb7adad9d621d3fc42ec555a0488
Andy Duncan <andy@duncaningram.com>
2014-07-17 20:26:30 -0400

    Evaluate using index/suffixarray as a drop in index search.

--- gozer.go
+++ gozer.go
@@ -4,6 +4,8 @@ import (
 	"github.com/go-martini/martini"
 	"github.com/jessevdk/go-flags"
 	"github.com/kr/fs"
+	"index/suffixarray"
+	"io/ioutil"
 	"fmt"
 	"os"
 //	"path/filepath"
@@ -30,7 +32,7 @@ func path_exists(path string) (bool, error) {
 	return false, err
 }
 
-func kwic_search(path string) {
+func index_search(path string, stf string) {
 	r, err := path_exists(path)
 	if err != nil {
 		fmt.Print("Error 1.\n")
@@ -54,22 +56,31 @@ func kwic_search(path string) {
 
 			fi := walker.Stat()
 			if fi.IsDir() {
-				kwic_search(walker.Path())
+				index_search(walker.Path(), stf)
 			} else {
 				fmt.Printf("Indexing file: %s\n", walker.Path())
+				data, err := ioutil.ReadFile(walker.Path())
+				if err != nil {
+					fmt.Fprintln(os.Stderr, err)
+					continue
+				}
+				index := suffixarray.New(data)
+				s := index.Lookup([]byte(stf), 1)
+				if len(s) > 0 {
+					fmt.Printf("Found match at: %s", s[0])
+				}
 			}
-
 		}
 	} else {
 		fmt.Printf("Path does not exist: %s", path)
 	}
 }
 
-func search(path string) {
+func search(path string, stf string) {
 	if path != "" {
-		kwic_search(path)
+		index_search(path, stf)
 	} else {
-		kwic_search("./")
+		index_search("./", stf)
 	}
 }
 
@@ -98,12 +109,12 @@ func main() {
 	if opts.Daemonize {
 		m := martini.Classic()
 		m.Get("/", func() string {
-			search(opts.Path)
+			search(opts.Path, opts.Search)
 			return "Search finished."
 		})
 		m.Run()
 	} else {
-		search(opts.Path)
+		search(opts.Path, opts.Search)
 	}
 
 }
