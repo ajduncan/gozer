350e50bfbcbde958446addf7924b0b02c6b89424
Andy Duncan <ajduncan@gmail.com>
2014-07-22 04:14:49 -0400

    Somewhat working index search.

--- gozer.go
+++ gozer.go
@@ -1,15 +1,17 @@
 package main
 
 import (
+	"bytes"
 	"github.com/go-martini/martini"
 	"github.com/jessevdk/go-flags"
 	"github.com/kr/fs"
+	"github.com/pmylund/go-cache"
 	"index/suffixarray"
 	"io/ioutil"
-	_ "github.com/mattn/go-sqlite3"
 	"fmt"
 	"os"
 	"strconv"
+	"time"
 )
 
 var opts struct {
@@ -18,6 +20,14 @@ var opts struct {
 	Path      string `short:"p" long:"path"   description:"Path to begin search"`
 	Search    string `short:"s" long:"search" description:"String to search for."`
 }
+
+type Keymaster struct {
+	index int
+	path string
+	context string
+}
+
+var gatekeeper = cache.New(30*time.Minute, 180*time.Second)
 
 
 func path_exists(path string) (bool, error) {
@@ -40,7 +50,6 @@ func index_search(path string, stf string) {
 	}
 
 	if r {
-		// fmt.Printf("Walking path: %s\n", path)
 		walker := fs.Walk(path)
 		for walker.Step() {
 			// if the path we're searching is the path we're on then let's
@@ -59,7 +68,6 @@ func index_search(path string, stf string) {
 			if fi.IsDir() {
 				index_search(walker.Path(), stf)
 			} else {
-				// fmt.Printf("Indexing file: %s\n", walker.Path())
 				data, err := ioutil.ReadFile(walker.Path())
 				if err != nil {
 					fmt.Fprintln(os.Stderr, err)
@@ -68,8 +76,21 @@ func index_search(path string, stf string) {
 				index := suffixarray.New(data)
 				s := index.Lookup([]byte(stf), 1)
 				if len(s) > 0 {
+					fmt.Printf("Indexing file: %s\n", walker.Path())
 					padding := s[0] + 25
 					if padding > cap(data) { padding = cap(data) }
+
+					if km_docs, found := gatekeeper.Get(stf); found {
+						di := Keymaster{s[0], walker.Path(), string(data[s[0]:padding])}
+						km_docs = append(km_docs.([]Keymaster), di)
+						gatekeeper.Set(stf, km_docs, 0)
+					} else {
+						var document_indexes []Keymaster
+						di := Keymaster{s[0], walker.Path(), string(data[s[0]:padding])}
+						document_indexes = append(document_indexes, di)
+						gatekeeper.Set(stf, document_indexes, 0)
+					}
+
 					fmt.Printf("%s(%s): %s\n", walker.Path(), strconv.Itoa(s[0]), data[s[0]:padding])
 				}
 			}
@@ -79,11 +100,30 @@ func index_search(path string, stf string) {
 	}
 }
 
-func search(path string, stf string) {
-	if path != "" {
-		index_search(path, stf)
+func km_string(km_doc []Keymaster) (km_string string) {
+    var result bytes.Buffer
+
+	for _, value := range km_doc {
+		result.WriteString(value.path + "(" + strconv.Itoa(value.index) + ") ... " + value.context + " ... \n")
+	}
+
+	return result.String()
+}
+
+func search(path string, stf string) (result string) {
+	if key, found := gatekeeper.Get(stf); found {
+		return km_string(key.([]Keymaster))
 	} else {
-		index_search("./", stf)
+		if path != "" {
+			index_search(path, stf)
+		} else {
+			index_search("./", stf)
+		}
+		if key, found := gatekeeper.Get(stf); found {
+			return km_string(key.([]Keymaster))
+		} else {
+			return "None found."
+		}
 	}
 }
 
@@ -115,12 +155,11 @@ func main() {
 			return "Main index... use /search/<key>."
 		})
 		m.Get("/search/:key", func (params martini.Params) string {
-			search(opts.Path, params["key"])
-			return "Search finished."
+			return search(opts.Path, params["key"])
 		})
 		m.Run()
 	} else {
-		search(opts.Path, opts.Search)
+		fmt.Printf("%s\n", search(opts.Path, opts.Search))
 	}
 
 }
